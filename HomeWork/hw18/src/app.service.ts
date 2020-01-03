88d28f041cb116380951c5420b5bb4d654f5ad37
Andrey Teslenko <andteslenko@gmail.com>
2020-01-03 16:52:34 +0200

    hw18

---
+++ HomeWork/hw18/src/app.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import {Formula} from  './formula.interface'
+
+@Injectable()
+export class AppService {
+
+  getHello() {
+    return
+  }
+
+  getPost() {
+    return
+  }
+  getDelete() {
+    return
+  }
+}
---
+++ HomeWork/hw18/src/formula.interface.ts
@@ -0,0 +1,41 @@
+export interface Formula {
+    parameters: IParam [];
+    id: number;
+    uuid: string;
+    createdAt: string;
+    updatedAt: string;
+    name: string;
+    formula: string;
+}
+
+  export interface IParam {
+    id: number;
+    validation: IValidation,
+    default: string;
+    name: string;
+    label: string;
+}
+
+export interface IValidation {
+    min: number;
+    max: number;
+    required: boolean;
+    readonly : boolean;
+    params: boolean;
+
+}
+// "parameters": [
+//     {
+//         "id": 703,
+//         "validation": {
+//             "min": 0,
+//             "max": 100,
+//             "required": null,
+//             "readonly": null,
+//             "params": null
+//         },
+//         "default": "10",
+//         "name": "WF",
+//         "label": "Waste Factor"
+//     },
+//
