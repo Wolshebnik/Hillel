a31c02cbfa4add2556ca07c870f79ad56a504696
Andrey Teslenko <andteslenko@gmail.com>
2020-01-17 17:40:59 +0200

    change hw22

--- HomeWork/hw22/src/teacher/schemas/teacher.schema.ts
+++ HomeWork/hw22/src/teacher/schemas/teacher.schema.ts
@@ -5,14 +5,14 @@ export const teacherSchema: any = new mongoose.Schema({
   name: String,
   age: Number,
   experience: Number,
-  studentsId: Array,
+  studentsId: Array
 });
 
 export interface Teacher {
   readonly name: string;
   readonly age: number;
   readonly experience: number;
-  readonly studentsId: ObjectId[];
+  readonly studentsId: [ObjectId];
 }
 
 export interface ITeacher extends Document, Teacher {}
--- HomeWork/hw22/src/teacher/services/teacher.service.ts
+++ HomeWork/hw22/src/teacher/services/teacher.service.ts
@@ -1,37 +1,79 @@
-import { Model } from 'mongoose';
-import { Inject, Injectable } from '@nestjs/common';
-import { ITeacher } from '../schemas/teacher.schema';
-import { IStudent } from '../../student/schemas/student.schema';
-import useRealTimers = jest.useRealTimers;
+import {Model} from 'mongoose';
+import {Inject, Injectable} from '@nestjs/common';
+import {ITeacher} from '../schemas/teacher.schema';
+import {IStudent} from '../../student/schemas/student.schema';
 
 @Injectable()
 export class TeacherService {
-  constructor(
-    @Inject('TeacherModelToken') private readonly teacherModel: Model<ITeacher>,
-    @Inject('StudentModelToken') private readonly studentModel: Model<IStudent>,
-  ) {}
+    constructor(
+        @Inject('TeacherModelToken') private readonly teacherModel: Model<ITeacher>,
+        @Inject('StudentModelToken') private readonly studentModel: Model<IStudent>,
+    ) {
+    }
 
-  async createTeacher(teacher: ITeacher): Promise<ITeacher> {
-    return await this.teacherModel.create(teacher);
-  }
+    async createTeacher(teacher: ITeacher): Promise<ITeacher> {
+        return await this.teacherModel.create(teacher);
+        // sent => {
+        //   "studentsId": [
+        //   "5e1252917e18279c241e2e1e",
+        //   "5e1252917e18279c241e2e1f",
+        //   "5e1d83904c29db41882e5b5f"
+        // ],
+        //     "name": "Sergey",
+        //     "age": 32,
+        //     "experience": 10,
+        //     "__v": 0
+        // }
+    }
 
-  async getAllTeacherWithStudents() {
-    const teachers = await this.teacherModel.find();
-    const students = await this.studentModel.find();
-    const all = teachers.map(item => {
-      const findStudents = item.studentsId.map(studentString => {
-        const findStudent = students.find(student => {
-          return student._id == studentString;
+    async getAllTeacherWithStudents() {
+        const teachers = await this.teacherModel.find();
+        const students = await this.studentModel.find();
+        const all = teachers.map(item => {
+            const addStudents = item.studentsId.map(studentString => {
+                const findStudent = students.find(student => {
+                    return student._id == studentString;
+                });
+                return findStudent;
+            });
+            item.studentsId = addStudents;
+            return item;
         });
-        return findStudent;
-      });
-      item.studentsId = findStudents;
-      return item;
-    });
-    return all;
-  }
+        return all;
+        // got => [
+        //     {
+        //       "studentsId": [
+        //         {
+        //           "_id": "5e1252917e18279c241e2e1e",
+        //           "name": "Andrey",
+        //           "lastName": "Teslenko",
+        //           "groupID": "5e1239514477b23ae89919a3"
+        //         },
+        //         {
+        //           "_id": "5e1252917e18279c241e2e1f",
+        //           "name": "Kirill",
+        //           "lastName": "Teslenko",
+        //           "groupID": "5e1239514477b23ae89919a3"
+        //         },
+        //         {
+        //           "_id": "5e1d83904c29db41882e5b5f",
+        //           "name": "Natasha",
+        //           "lastName": "Teslenko",
+        //           "groupID": "5e1239514477b23ae89919a3",
+        //           "__v": 0
+        //         }
+        //       ],
+        //       "_id": "5e21cd753c3bdf112ce49a71",
+        //       "name": "Sergey",
+        //       "age": 32,
+        //       "experience": 10,
+        //       "__v": 0
+        //     }
+        //     ]
+    }
 
-  async getAllTeacher() {
-    return await this.teacherModel.find();
-  }
+    async getAllTeacher() {
+        return await this.teacherModel.find();
+    }
+
 }
