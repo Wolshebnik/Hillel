237e65b89024c66bc43cabafcb16a4716d9de7ce
Dron <andteslenko@gmail.com>
2020-08-07 20:02:25 +0300

    Andrey_Teslenko_Fridge

--- React/HW1/cars/src/App.js
+++ React/HW1/cars/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-import { v4 as uuidv4 } from 'uuid';
-
+import { v4 as uuidv4 } from "uuid";
 
 import "./App.css";
 import InputField from "./components/inputField";
@@ -9,8 +8,10 @@ import Button from "./components/button";
 
 export default class App extends Component {
   state = {
-    filter: null,
-    newCar: {model:'', color:''},
+    select:null,
+    doneFilter: false,
+    filter: [],
+    newCar: { model: "", color: "" },
     cars: [
       {
         id: uuidv4(),
@@ -35,37 +36,59 @@ export default class App extends Component {
     ],
   };
 
+  componentDidMount() {
+    const filter = this.state.cars.map((val) => val.color);
+    this.setState({ filter: [...new Set(filter)] });
+
+
+  }
+
   deleteItem = (id) => {
     const idx = this.state.cars.findIndex((el) => el.id === id);
-    const cars = [...this.state.cars.slice(0, idx), ...this.state.cars.slice(idx + 1)]
+    const cars = [
+      ...this.state.cars.slice(0, idx),
+      ...this.state.cars.slice(idx + 1),
+    ];
 
-    this.setState({cars});
+    this.setState({ cars });
   };
   createItem = (event) => {
     const { placeholder, value } = event.target;
-    const newCar = {[placeholder]: value}
+    const newCar = { [placeholder]: value };
 
     this.setState({
-      newCar: {...this.state.newCar,...newCar}
+      newCar: { ...this.state.newCar, ...newCar },
     });
   };
 
   addItem = (e) => {
     e.preventDefault();
-    if (this.state.newCar.model === "" || this.state.newCar.color === "") return;
-    const newCar = {id: uuidv4(), ...this.state.newCar}
+    this.setState({ doneFilter: false });
+    if (this.state.newCar.model === "" || this.state.newCar.color === "")
+      return;
+    const newCar = { id: uuidv4(), ...this.state.newCar };
 
-    this.setState({cars:[...this.state.cars, newCar]})
-    this.setState({newCar: {model: '', color: ''}})
-
+    this.setState({ cars: [...this.state.cars, newCar] });
+    this.setState({ newCar: { model: "", color: "" } });
   };
+
+  changeSelect=(e) => {
+    console.log(e.target.value)
+    // this.setState({select:this.state.filter[0]})
+    // console.log(this.state.select)
+  this.setState({select:e.target.value})
+
+  }
 
   onFilter = () => {
     const allBlue = this.state.cars.filter((blue) => blue.color === "blue");
-   this.setState({filter:allBlue})
-    console.log(this.state.filter);
+    this.setState({ filter: allBlue });
+    this.setState({ doneFilter: true });
+
   };
   render() {
+    // console.log(this.state.select)
+    // console.log(this.state.filter[0])
     return (
       <div className="App">
         <ul>
@@ -89,14 +112,21 @@ export default class App extends Component {
             />
 
             <Button text={"Add"} />
-            <Button text={"Filter blue"} func={() => this.onFilter()} />
           </form>
-          {this.state.filter && this.state.filter.map(check=> <ListItem key={`${check.id}_${check.color}`} carsArray={check}
-              btn={'no'}
-              />)}
+          <select onChange={this.changeSelect}>
+            {this.state.filter.map(e => <option key={uuidv4()} value={e}>{e}</option>)}
+          </select>
+          <Button text={"Filter blue"} func={() => this.onFilter()} />
+          {this.state.doneFilter &&
+            this.state.filter.map((check) => (
+              <ListItem
+                key={`${check.id}_${check.color}`}
+                carsArray={check}
+                btn={"no"}
+              />
+            ))}
         </ul>
       </div>
     );
   }
-};
-
+}
